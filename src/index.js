33ba69c1bf0a355d37041d17fca6a860d288df15
kwonyijun13 <kwonyijun13@hotmail.com>
2022-10-31 03:20:20 +0900

    last changes

--- src/index.js
+++ src/index.js
@@ -65,7 +65,6 @@ io.on('connection', (socket) => {
 
     socket.on('sendLocation', (coords, callback) => {
         const user = getUser(socket.id)
-
         // GOOGLE MAP
         // io.to(user.chatroom).emit('locationMessage', generateLocationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
         // NAVER MAP
